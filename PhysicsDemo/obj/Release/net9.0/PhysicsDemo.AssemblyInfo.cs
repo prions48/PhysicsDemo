34b336ff3d6b2f9fa6917b59a84bb2af30971f68
Alejandro Enriquez <prions48@gmail.com>
2025-11-30 15:46:30 -0600

    a little more frontend for game winning and game access

--- PhysicsDemo/obj/Release/net9.0/PhysicsDemo.AssemblyInfo.cs
+++ PhysicsDemo/obj/Release/net9.0/PhysicsDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhysicsDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d4b83ec5e121c05f03c7c6a303de0787d5fd55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbb7a358c6f7df6dc9173220df2615043510315")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhysicsDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhysicsDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
