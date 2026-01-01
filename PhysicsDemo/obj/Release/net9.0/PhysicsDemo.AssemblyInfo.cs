2790810ad1554168f84b1b71845d0b871ed08ddf
Alejandro Enriquez <prions48@gmail.com>
2026-01-01 16:57:23 -0600

    added persistance across crashing/refreshing for guest players

--- PhysicsDemo/obj/Release/net9.0/PhysicsDemo.AssemblyInfo.cs
+++ PhysicsDemo/obj/Release/net9.0/PhysicsDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhysicsDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10fe7abdf557d9b879b5702bdb41ebad5eaa024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+550402fc49c770a20d2eb9ffce1649be988933cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhysicsDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhysicsDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
